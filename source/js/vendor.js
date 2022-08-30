5f86978c43fd670441e5cc0558af58f3f986dc1c
m14ai1 <m14ai1@yandex.ru>
2022-08-30 05:08:21 +0300

    figured out a bit with Swiper can be styled

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,2 +1,12 @@
 // Swiper 7.4.1
-// import './vendor/swiper';
+import Swiper from './vendor/swiper-bundle';
+
+let swiper = new Swiper('.coaches-slider', {
+  slidesPerView: 4,
+  spaceBetween: 30,
+  freeMode: true,
+  navigation: {
+    nextEl: '.coaches-slider__next',
+    prevEl: '.coaches-slider__prev',
+  },
+});
