c1055312a9baef496de4a0f59a8ea7cdfb53f355
m14ai1 <m14ai1@yandex.ru>
2022-08-31 02:25:54 +0300

    coaches card styling

--- source/js/vendor.js
+++ source/js/vendor.js
@@ -1,12 +1,14 @@
 // Swiper 7.4.1
 import Swiper from './vendor/swiper-bundle';
 
-let swiper = new Swiper('.coaches-slider', {
+let swiper = new Swiper('.coaches-slider__swiper', {
   slidesPerView: 4,
-  spaceBetween: 30,
+  spaceBetween: 40,
   freeMode: true,
   navigation: {
     nextEl: '.coaches-slider__next',
     prevEl: '.coaches-slider__prev',
   },
 });
+
+export {swiper};
